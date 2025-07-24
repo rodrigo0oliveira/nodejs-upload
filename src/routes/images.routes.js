295b6477a2ccd08d5f1b57b52896f91dedcc0374
Rodrigo <rodrigogitversion@gmail.com>
2025-07-24 13:18:03 -0300

    docs: document upload routes

--- src/routes/images.routes.js
+++ src/routes/images.routes.js
@@ -6,8 +6,56 @@ const multerMiddleware = require('../middleware/upload-middleware')
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Images
+ *   description: Images Endpoints
+ */
+
+
+/**
+ * @swagger
+ * /api/images:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Upload Image.
+ *     tags: [Images]
+ *     responses:
+ *       201:
+ *         description: Successful response - Your image was upload successfuly!
+ *       401:
+ *         description: Not Authorized - User are not authenticated or is not admin!
+ *       500:
+ *         description: Internal Server Error!
+ *     requestBody:
+ *       content:
+ *          image/png:
+ *              schema:
+ *                  type: string
+ *                  format: binary
+ *            
+ */
 router.post('/',authMiddleware,adminMiddleware,multerMiddleware.single('image'),uploadImage);//upload image - only admin
 
+/**
+ * @swagger
+ * /api/images:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Fetch Images.
+ *     tags: [Images]
+ *     responses:
+ *       200:
+ *         description: Successful response - Fetch images was done!
+ *       204:
+ *         description: Succesful response - Fetch image was done but no one image was found!
+ *       500:
+ *         description: Internal Server Error!
+ *            
+ */
 router.get('/',authMiddleware,fetchImages); //get images - need auth
 
 module.exports = router;
